acfeba841a7382c9a6534312b347a0541acdbbd8
Dead_moon <58658937+GloryToMoon@users.noreply.github.com>
2020-02-21 03:07:09 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>HACKED</title>
+<style>
+div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: Avantgarde;
+    font-size: 60%;
+};
+</style>
+</head>
+<body >
+<div>
+<img src='YOBA.png'  width="50%" height="50%">
+<h1>This site hacked by russian team YOBA.</h1>
+<h1>Q: -Why this happend?</h1>
+<h1>A: -Why not?</h1>
+<h1>Q: -Hey, thats my server! How I get it back?</h1>
+<h1>A: -Say a magic word.</h1>
+<h1>A: -How can I contact you?</h1>
+<h1><a href="https://www.google.at/search?q=wojak+doomer&authuser=0&hl=ru&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiFqMDaq-HnAhUwQEEAHbsiDYwQ_AUoAXoECAwQAw&biw=1920&bih=966">YOBA</a></h1>
+</div>
+</body>
+</html>
